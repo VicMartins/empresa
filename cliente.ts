b8f0dd87433b8f0d30f9cf4277669a4096695bb6
vicmartins <vic.martins0907@gmail.com>
2019-11-19 17:55:22 +0000

    commit inicial

---
+++ cliente.ts
@@ -0,0 +1,20 @@
+ reference path="pessoa.ts"
+namespace empresa{
+    export class Cliente extends Pessoa {
+        private _codCliente:number;
+        private _credito:number;
+
+        public getCodCliente(){
+            return this._codCliente;
+        }
+        public setCodCliente(codigo:number){
+            this._codCliente = codigo;
+        }
+        public getCredito(){
+            return this._Credito;
+        }
+        public setCredito(credito:number){
+            this._credito =  credito;
+        }
+    }
+}
